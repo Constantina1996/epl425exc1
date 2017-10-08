d6386529575f5564f40de49e7cf97aaaad6d47ee
mangel033 <32542129+mangel033@users.noreply.github.com>
2017-10-08 16:24:46 +0300

    Add files via upload

--- Assignment1/src/MultiTCPClient.java
+++ Assignment1/src/MultiTCPClient.java
@@ -26,25 +26,30 @@ public class MultiTCPClient {
 		private String serverbuffer;
 		private String message;
 		private String response;
-		private static int counter=1;
+		private int id;
 
-		public TCPWorker(Socket socket) {
+		public TCPWorker(Socket socket,int id) {
 			this.socket = socket;
 			this.serverbuffer = "";
+			this.id=id;
 		}
 
 		@Override
 		public void run() {
-		
+
 			try {
-				this.output = new DataOutputStream(this.socket.getOutputStream());
-				this.server = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
-				this.reader = new BufferedReader(new InputStreamReader(System.in));
-				this.message = "Hello, IP client: "+this.socket.getInetAddress()+"Port: "+this.socket.getLocalPort()+"User: "+this.counter+++"\n";
-				//this.message=reader.readLine() + System.lineSeparator();
-				this.output.writeBytes(message);
-				this.response = server.readLine();
-				System.out.println("[" + new Date() + "] Received: " + response);
+				for (int i = 0; i < 300; i++) {
+					this.output = new DataOutputStream(this.socket.getOutputStream());
+					this.server = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+					this.reader = new BufferedReader(new InputStreamReader(System.in));
+					this.message = "Hello, IP client: " + this.socket.getInetAddress() + " Port: "
+							+ this.socket.getLocalPort() + " User: " + this.id + "\n";
+					// this.message=reader.readLine() + System.lineSeparator();
+					this.output.writeBytes(message);
+					this.response = server.readLine();
+					System.out.println("[" + new Date() + "] Received: " + response);
+				}
+				
 				this.socket.close();
 			} catch (IOException e) {
 				e.printStackTrace();
@@ -54,15 +59,15 @@ public class MultiTCPClient {
 
 	}
 
-	public static ExecutorService TCP_WORKER_SERVICE = Executors.newFixedThreadPool(300);
+	public static ExecutorService TCP_WORKER_SERVICE = Executors.newFixedThreadPool(10);
 
 	public static void main(String[] args) {
 		Socket socket = null;
 		int i;
 		try {
-			for (i = 0; i < 300; i++) {
-				socket = new Socket("34.212.247.247", 80);
-				TCP_WORKER_SERVICE.submit(new TCPWorker(socket));
+			for (i = 1; i <= 10; i++) {
+				socket = new Socket("34.208.28.211", 80);
+				TCP_WORKER_SERVICE.submit(new TCPWorker(socket,i));
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
