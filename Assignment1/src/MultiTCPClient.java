52cc121790a3f6799bec9ecce8448ab1529ecb2d
mangel033 <32542129+mangel033@users.noreply.github.com>
2017-10-08 18:29:40 +0300

    Add files via upload

--- Assignment1/src/MultiTCPClient.java
+++ Assignment1/src/MultiTCPClient.java
@@ -17,7 +17,8 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class MultiTCPClient {
-	static int client=0;
+	static int client = 0;
+
 	private static class TCPWorker implements Runnable {
 
 		private Socket socket;
@@ -28,18 +29,22 @@ public class MultiTCPClient {
 		private String message;
 		private String response;
 		private int id;
+		private int c;
+		private int r;
 
-		public TCPWorker(Socket socket,int id) {
+		public TCPWorker(Socket socket, int id, int c, int r) {
 			this.socket = socket;
 			this.serverbuffer = "";
-			this.id=id;
+			this.id = id;
+			this.c = c;
+			this.r = r;
 		}
 
 		@Override
 		public void run() {
 
 			try {
-				for (int i = 0; i < 50; i++) {
+				for (int i = 0; i < r; i++) {
 					this.output = new DataOutputStream(this.socket.getOutputStream());
 					this.server = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
 					this.reader = new BufferedReader(new InputStreamReader(System.in));
@@ -50,11 +55,11 @@ public class MultiTCPClient {
 					this.response = server.readLine();
 					System.out.println("[" + new Date() + "] Received: " + response);
 				}
-				this.message="FINISHED";
+				this.message = "FINISHED";
 				this.output.writeBytes(message);
 				client++;
 				this.socket.close();
-				if(client==2)
+				if (client == c)
 					System.exit(0);
 			} catch (IOException e) {
 				e.printStackTrace();
@@ -69,16 +74,20 @@ public class MultiTCPClient {
 	public static void main(String[] args) {
 		Socket socket = null;
 		int i;
+		String cl = args[0];
+		String req = args[1];
+		int c = Integer.parseInt(cl);
+		int r = Integer.parseInt(req);
+
 		try {
-			for (i = 1; i <= 2; i++) {
+			for (i = 1; i <= c; i++) {
 				socket = new Socket("34.208.28.211", 80);
-				TCP_WORKER_SERVICE.submit(new TCPWorker(socket,i));
+				TCP_WORKER_SERVICE.submit(new TCPWorker(socket, i, c, r));
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	
-	}
 
+	}
 
 }
